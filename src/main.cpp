8b7e555340d83fa51338dc7f96cf6bda6ba7657b
quiks <quiksilver4210@gmail.com>
2019-10-02 22:23:16 +1000

    Merge branch 'master' of /home/quiks/Desktop/SelSQL with conflicts.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
 
 #include "parser.cpp"
+#include "Engine/Headers/MainEngine.h"
 int main() {
-    char request[] = "CREATE TABLE name(ID INT);";
-    parse_request(request);
+    std::string request;
+    std::getline(std::cin,request);
+
+    parse_request(request.c_str());
+
+
+
+//    auto mainEngine = new MainEngine();
+//    Table t, c;
+//    Variable v;
+//    v.name = "ID";
+//    v.type = INT;
+//    t.name = "TEST";
+//    std::vector<Variable> vector = {v};
+//    t.fields = vector;
+//
+//    mainEngine->CreateTable(&t);
+//    c = *mainEngine->ShowCreateTable("TEST");
 
     return 0;
 }
