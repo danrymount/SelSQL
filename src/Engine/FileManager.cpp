e3a9c234e2dcd281de3eedbccdcfc2e3850a33be
quiks <quiksilver4210@gmail.com>
2019-10-02 19:11:39 +1000

    reformat code

--- src/Engine/FileManager.cpp
+++ src/Engine/FileManager.cpp
@@ -3,19 +3,18 @@
 
 void FileManager::WriteMetaData(Table* table) {
     files_[table->name]->write(table->name.c_str(), sizeof(table->name.size()));
-    files_[table->name]->seekp(Constants::META_BLOCK_COLUMN_NAME_SIZE);
+    files_[table->name]->seekp(Constants::MD_COLUMN_NAME_SIZE);
     int field_size = table->fields.size();
-    files_[table->name]->write(reinterpret_cast<char*>(&field_size), Constants::META_BLOCK_TABLE_COLUMN_AMOUNT_SIZE);
+    files_[table->name]->write(reinterpret_cast<char*>(&field_size), Constants::MD_TABLE_COLUMN_AMOUNT_SIZE);
     for (auto& field : table->fields) {
-        files_[table->name]->write(field.name.c_str(), Constants::META_BLOCK_COLUMN_NAME_SIZE);
+        files_[table->name]->write(field.name.c_str(), Constants::MD_COLUMN_NAME_SIZE);
         int constr_size = field.constraints.size();
         int type = Type(field.type);
-        files_[table->name]->write(reinterpret_cast<char*>(&type), Constants::META_BLOCK_COLUMN_TYPE_SIZE);
-        files_[table->name]->write(reinterpret_cast<char*>(&constr_size),
-                                   Constants::META_BLOCK_COLUMN_CONSTR_AMOUNT_SIZE);
+        files_[table->name]->write(reinterpret_cast<char*>(&type), Constants::MD_COLUMN_TYPE_SIZE);
+        files_[table->name]->write(reinterpret_cast<char*>(&constr_size), Constants::MD_COLUMN_CONSTR_AMOUNT_SIZE);
         for (auto constr : field.constraints) {
             int contr_type = Constraint(constr);
-            files_[table->name]->write(reinterpret_cast<char*>(&contr_type), Constants::META_BLOCK_COLUMN_CONSTR_SIZE);
+            files_[table->name]->write(reinterpret_cast<char*>(&contr_type), Constants::MD_COLUMN_CONSTR_SIZE);
         }
     }
     files_[table->name]->close();
@@ -24,26 +23,25 @@ void FileManager::WriteMetaData(Table* table) {
 void FileManager::ReadMetaData(std::string table_name) {
     Table table;
     std::vector<Variable> fields;
-    char name[Constants::META_BLOCK_TABLE_NAME_SIZE];
+    char name[Constants::MD_TABLE_NAME_SIZE];
     int column_size;
-    files_[table_name]->read(name, Constants::META_BLOCK_TABLE_NAME_SIZE);
-    files_[table_name]->read(reinterpret_cast<char*>(&column_size), Constants::META_BLOCK_TABLE_COLUMN_AMOUNT_SIZE);
+    files_[table_name]->read(name, Constants::MD_TABLE_NAME_SIZE);
+    files_[table_name]->read(reinterpret_cast<char*>(&column_size), Constants::MD_TABLE_COLUMN_AMOUNT_SIZE);
     for (int i = 0; i < column_size; ++i) {
         Variable var;
-        char var_name[Constants::META_BLOCK_COLUMN_NAME_SIZE];
+        char var_name[Constants::MD_COLUMN_NAME_SIZE];
         int type = 0;
         int constr_size = 0;
         std::vector<Constraint> constraint;
-        files_[table_name]->read(var_name, Constants::META_BLOCK_COLUMN_NAME_SIZE);
-        files_[table_name]->read(reinterpret_cast<char*>(&type), Constants::META_BLOCK_COLUMN_TYPE_SIZE);
-        files_[table_name]->read(reinterpret_cast<char*>(&constr_size),
-                                 Constants::META_BLOCK_COLUMN_CONSTR_AMOUNT_SIZE);
+        files_[table_name]->read(var_name, Constants::MD_COLUMN_NAME_SIZE);
+        files_[table_name]->read(reinterpret_cast<char*>(&type), Constants::MD_COLUMN_TYPE_SIZE);
+        files_[table_name]->read(reinterpret_cast<char*>(&constr_size), Constants::MD_COLUMN_CONSTR_AMOUNT_SIZE);
         var.name = std::string(var_name);
         var.type = Type(type);
 
         for (int j = 0; j < constr_size; ++j) {
             int constr_type = 0;
-            files_[table_name]->read(reinterpret_cast<char*>(&constr_type), Constants::META_BLOCK_COLUMN_CONSTR_SIZE);
+            files_[table_name]->read(reinterpret_cast<char*>(&constr_type), Constants::MD_COLUMN_CONSTR_SIZE);
             constraint.emplace_back(Constraint(constr_type));
         }
         var.constraints = constraint;
--- src/Utils/Headers/Constants.h
+++ src/Utils/Headers/Constants.h
@@ -6,12 +6,18 @@
 class Constants {
    public:
     inline static const std::string FILE_TYPE = ".selDB";
-    static const int META_BLOCK_TABLE_NAME_SIZE = 256;
-    static const int META_BLOCK_TABLE_COLUMN_AMOUNT_SIZE = sizeof(int);
-    static const int META_BLOCK_COLUMN_NAME_SIZE = 256;
-    static const int META_BLOCK_COLUMN_TYPE_SIZE = sizeof(int);
-    static const int META_BLOCK_COLUMN_CONSTR_SIZE = sizeof(int);
-    static const int META_BLOCK_COLUMN_CONSTR_AMOUNT_SIZE = 10;
+    static const int MD_TABLE_NAME_SIZE = 256;
+    static const int MD_TABLE_COLUMN_AMOUNT_SIZE = sizeof(int);
+    static const int MD_TABLE_COLUMN_AMOUNT_MAX = 16;
+    static const int MD_COLUMN_NAME_SIZE = 256;
+    static const int MD_COLUMN_TYPE_SIZE = sizeof(int);
+    static const int MD_COLUMN_CONSTR_SIZE = sizeof(int);
+    static const int MD_COLUMN_CONSTR_AMOUNT_SIZE = sizeof(int);
+    static const int MD_COLUMN_CONSTR_AMOUNT_MAX = 10;
+    static const int DATA_PAGE_START_POS = MD_TABLE_NAME_SIZE + MD_TABLE_COLUMN_AMOUNT_SIZE +
+                                           MD_TABLE_COLUMN_AMOUNT_MAX * (MD_COLUMN_NAME_SIZE + MD_COLUMN_TYPE_SIZE +
+                                                                         MD_COLUMN_CONSTR_AMOUNT_SIZE +
+                                                                         MD_COLUMN_CONSTR_AMOUNT_MAX * MD_COLUMN_CONSTR_SIZE);
     static const int DATA_PAGE_SIZE = 4 * 1024;
 };
 
