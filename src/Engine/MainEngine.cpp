d1d56b60d9a8ed9e9873149ec172a56c50a5b566
quiks <quiksilver4210@gmail.com>
2019-10-03 01:23:09 +1000

    Update DeleteTable

--- src/Engine/MainEngine.cpp
+++ src/Engine/MainEngine.cpp
@@ -3,15 +3,19 @@
 //
 
 #include "Headers/MainEngine.h"
-int MainEngine::CreateTable(Table* table) { return file_manager_->CreateFile(table); }
+int MainEngine::CreateTable(Table* table) {
+    return file_manager_->CreateFile(table);
+}  // returns 0 if table is created and 1 if table exists
 
 Table* MainEngine::ShowCreateTable(std::string table_name) {
     if (file_manager_->OpenFile(table_name)) {
         return nullptr;
     }
     return file_manager_->GetTableData(table_name);
-}
+}  // returns Table* if ok or nullptr if table doesnt exist
 
-int MainEngine::DropTable(std::string table_name) { return 0; }
+int MainEngine::DropTable(std::string table_name) {
+    return file_manager_->DeleteTable(table_name);
+}  // returns 0 if table is dropped and non-zero if table doesnt exist
 
 MainEngine::MainEngine() { file_manager_ = new FileManager(); }
