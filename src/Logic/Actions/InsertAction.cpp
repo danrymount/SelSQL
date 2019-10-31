3065879a183fe9c826acbc494d5e22f04516c271
Anton Mikhailenko <toxaxab27@gmail.com>
2019-10-31 15:47:14 +1000

    HOT FIX.HOT FIX FOR INSERT AND CHECK CHAR LENGTH.

--- src/Logic/Actions/InsertAction.cpp
+++ src/Logic/Actions/InsertAction.cpp
@@ -7,7 +7,7 @@
 
 Message InsertAction::execute(std::shared_ptr<BaseActionNode> root) {
     root->accept(getTreeVisitor().get());
-    auto v = static_cast<InsertVisitor*>(getTreeVisitor().get());
+    auto v = static_cast<InsertVisitor *>(getTreeVisitor().get());
     auto columns = v->getColumns();
     auto values = v->getValues();
 
@@ -65,18 +65,32 @@ Message InsertAction::execute(std::shared_ptr<BaseActionNode> root) {
     for (int i = 0; i < table->getFields().size(); i++) {
         std::pair<std::string, std::string> curColValue;
         auto tableFiels = cursor.first->getFields()[i];
+
+        std::string val = "";
         if (!columns.empty()) {
             if (columns[0] == "*") {
-                if (!tableFiels.second.size || tableFiels.second.size >= values[i].length()) {
+                if (tableFiels.second.type == Type::TYPE_CHAR) {
+                    val = values[i].substr(1, values[i].length() - 2);
+                } else {
+                    val = values[i];
+                }
+                if (!tableFiels.second.size || tableFiels.second.size >= val.length()) {
                     columnsValues.emplace_back(std::make_pair(cursor.first->getFields()[i].first, values[i]));
                     continue;
+                } else {
+                    return Message(ErrorConstants::ERR_BIG_CHAR);
                 }
             }
         }
         auto it = std::find(columns.begin(), columns.end(), tableFiels.first);
         if (it != columns.end()) {
             auto index = std::distance(columns.begin(), it);
-            if (!tableFiels.second.size || tableFiels.second.size >= values[index].length()) {
+            if (tableFiels.second.type == Type::TYPE_CHAR) {
+                val = values[i].substr(1, values[index].length() - 2);
+            } else {
+                val = values[i];
+            }
+            if (!tableFiels.second.size || tableFiels.second.size >= val.length()) {
                 columnsValues.emplace_back(std::make_pair(columns[index], values[index]));
             }
         } else {
