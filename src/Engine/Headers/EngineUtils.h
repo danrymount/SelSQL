153da541c4bad9133ffa3eb453fcfa2309cd4a79
quiks <quiksilver4210@gmail.com>
2019-11-04 21:54:21 +1000

    Refactor

--- src/Engine/Headers/EngineUtils.h
+++ src/Engine/Headers/EngineUtils.h
@@ -5,12 +5,13 @@
 #ifndef SELSQL_ENGINEUTILS_H
 #define SELSQL_ENGINEUTILS_H
 
+#include <cstring>
 #include <filesystem>
 #include <fstream>
 #include <memory>
 #include "../../Utils/Headers/Constants.h"
+#include "../../Utils/Structures/Data/DataBlock.h"
 #include "../../Utils/Structures/Data/Table.h"
-#include <cstring>
 #ifdef __WIN32
 #define DIR_SEPARATOR '\\'
 #elif __linux
@@ -29,16 +30,15 @@ struct DB_FILE {
     void close();
     int isOpen();
 };
-//int CheckFileHashSum(const std::shared_ptr<DB_FILE>& file);
+
 void WriteIntToFile(std::fstream* file, int value);
 int ReadIntFromFile(std::fstream* file);
 int GetFileSize(std::fstream* file);
-buffer_data GetTableBuffer(Table* table);
-std::shared_ptr<Table> ReadTableFromBuffer(char* data);
 
-//double CalcHashSum(const std::shared_ptr<std::fstream>& file);
-//std::shared_ptr<DB_FILE> FindLastVersion(const std::string& table_name, int max_v);
-//int GetVersion(const std::string& file_name);
+buffer_data GetTableBuffer(Table* table);
+buffer_data GetDataBlockBuffer(DataBlock* data_block);
+std::shared_ptr<DataBlock> ReadDataBlockFromBuffer(char* data);
+std::shared_ptr<Table> ReadTableFromBuffer(char* data);
 
 
 class FileNotOpened{};
