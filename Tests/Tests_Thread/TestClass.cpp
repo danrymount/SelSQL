74ca3f6b60287a5c8e6a029b40beb8f04dd14a8f
antons201 <penugin2012@yandex.ru>
2019-12-19 15:34:17 +1000

    Update tests

--- Tests/Tests_Thread/TestClass.cpp
+++ Tests/Tests_Thread/TestClass.cpp
@@ -12,18 +12,31 @@ TEST(SERVER_TEST_THREAD, THREAD_TEST_INSERT_IN_DIFFERENT_THREADS_IN_DIFFERENT_TA
     std::vector<Request> request1{{"begin;", "Success"},
                                   {"insert into t values(1, 'Vasya', 'Gorod',  7.5);", "Success"},
                                   {"insert into t values(1, 'Vasya', 'Gorod',  7.5);", "Success"},
-                                  {"select * from t;", ""},
+                                  {"select * from t;",
+                                   "\nid|name   |city   |age     |\n"
+                                   "1 |'Vasya'|'Gorod'|7.500000|\n"
+                                   "1 |'Vasya'|'Gorod'|7.500000|\n"},
                                   {"commit;", "Success"}};
     std::vector<Request> request2{{"begin;", "Success"},
                                   {"insert into t1 values(1, 'Vasya', 'Gorod',  7.5);", "Success"},
                                   {"insert into t1 values(1, 'Vasya', 'Gorod',  7.5);", "Success"},
-                                  {"select * from t1;", ""},
+                                  {"select * from t1;",
+                                   "\nid|name   |city   |age     |\n"
+                                   "1 |'Vasya'|'Gorod'|7.500000|\n"
+                                   "1 |'Vasya'|'Gorod'|7.500000|\n"},
                                   {"commit;", "Success"}};
     std::thread client1(TestUtils::checkRequests, request1);
     std::thread client2(TestUtils::checkRequests, request2);
     client1.join();
     client2.join();
-    TestUtils::checkRequests({{"select * from t;", ""}, {"select * from t1;", ""}});
+    TestUtils::checkRequests({{"select * from t;",
+                               "\nid|name   |city   |age     |\n"
+                               "1 |'Vasya'|'Gorod'|7.500000|\n"
+                               "1 |'Vasya'|'Gorod'|7.500000|\n"},
+                              {"select * from t1;",
+                               "\nid|name   |city   |age     |\n"
+                               "1 |'Vasya'|'Gorod'|7.500000|\n"
+                               "1 |'Vasya'|'Gorod'|7.500000|\n"}});
 }
 
 TEST(SERVER_TEST_THREAD, THREAD_TEST_UPDATE_NOT_YET_INSERTED_VALUES) {
@@ -77,12 +90,15 @@ TEST(SERVER_TEST_THREAD, THREAD_TEST_DELETE_NOT_YET_INSERTED_VALUES) {
                               {"insert into t4 values(1, 'Vasya', 'Gorod',  7.5);", "Success"}});
     std::vector<Request> request1{{"begin;", "Success"},
                                   {"insert into t4 values(2, 'Vas', 'Gorodok',  4.5);", "Success"},
-                                  {"select * from t4;", ""},
+                                  {"select * from t4;",
+                                   "\nid|name   |city     |age     |\n"
+                                   "1 |'Vasya'|'Gorod'  |7.500000|\n"
+                                   "2 |'Vas'  |'Gorodok'|4.500000|\n"},
                                   {"commit;", "Success"}};
     std::vector<Request> request2{{"begin;", "Success"},
                                   {"delete from t4 where id = 1;", "Success"},
                                   {"delete from t4 where id = 2;", "Success"},
-                                  {"select * from t4;", ""},
+                                  {"select * from t4;", "Success"},
                                   {"commit;", "Success"}};
     // в этом тесте предполагается, что делит у второго клиента выполнится во время работы первого и соответственно не
     // удаит значение с айди равным 2
@@ -90,6 +106,9 @@ TEST(SERVER_TEST_THREAD, THREAD_TEST_DELETE_NOT_YET_INSERTED_VALUES) {
     std::thread client2(TestUtils::checkRequests, request2);
     client1.join();
     client2.join();
+    TestUtils::checkRequests({{"select * from t4;",
+                               "\nid|name |city     |age     |\n"
+                               "2 |'Vas'|'Gorodok'|4.500000|\n"}});
 }
 
 TEST(SERVER_TEST_THREAD, THREAD_TEST_UPDATE_IN_TWO_TRANSACTIONS) {
@@ -121,21 +140,33 @@ TEST(SERVER_TEST_THREAD, THREAD_TEST_UPDATE_IN_TWO_TRANSACTIONS) {
 TEST(SERVER_TEST_THREAD, THREAD_TEST_UPDATE_DIFFERENT_VALUES) {
     TestUtils::clear();
     TestUtils::checkRequests({{"create table t6(id int, name char(255), city char(255), age float);", "Success"},
-                              {"insert into t values(1, 'Vasya', 'Gorod',  7.5);", "Success"}});
+                              {"insert into t6 values(1, 'Vasya', 'Gorod',  7.5);", "Success"}});
     std::vector<Request> request1{{"begin;", "Success"},
                                   {"insert into t6 values(2, 'Vas', 'Gorodok',  4.5);", "Success"},
                                   {"update t6 set id = 10 where id = 2;", "Success"},
-                                  {"select * from t6;", ""},
+                                  {"select * from t6;",
+                                   "\nid|name   |city     |age     |\n"
+                                   "1 |'Vasya'|'Gorod'  |7.500000|\n"
+                                   "10|'Vas'  |'Gorodok'|4.500000|\n"},
                                   {"commit;", "Success"}};
-    std::vector<Request> request2{{"begin;", "Success"},    {"delete from t where id = 10;", "Success"},
-                                  {"select * from t6;", ""}, {"update t set id = 9 where id = 1;", "Success"},
-                                  {"select * from t6;", ""}, {"commit;", "Success"}};
+    std::vector<Request> request2{{"begin;", "Success"},
+                                  {"delete from t6 where id = 10;", "Success"},
+                                  {"select * from t6;",
+                                   "\nid|name   |city   |age     |\n"
+                                   "1 |'Vasya'|'Gorod'|7.500000|\n"},
+                                  {"update t6 set id = 9 where id = 1;", "Success"},
+                                  {"select * from t6;", "\nid|name   |city   |age     |\n"
+                                                        "9 |'Vasya'|'Gorod'|7.500000|\n"},
+                                  {"commit;", "Success"}};
     // в этом тесте предполагается, что делит у второго клиента выполнится во время работы первого и соответственно не
     // удаит значение с айди равным 10 а также выполнятся апдейты в обоих транзакциях, т.к. они апдейтят разные поля
     std::thread client1(TestUtils::checkRequests, request1);
     std::thread client2(TestUtils::checkRequests, request2);
     client1.join();
     client2.join();
+    TestUtils::checkRequests({{"select * from t6;", "\nid|name   |city     |age     |\n"
+                                                    "10|'Vas'  |'Gorodok'|4.500000|\n"
+                                                    "9 |'Vasya'|'Gorod'  |7.500000|\n"}});
 }
 
 TEST(SERVER_TEST_THREAD, THREAD_TEST_DELETE_INTO_TWO_TRANSACTIONS) {
@@ -165,15 +196,22 @@ TEST(SERVER_TEST_THREAD, THREAD_TEST_UPDATE_INTO_TWO_TRANSACTIONS_PARALLEL_TREAD
                               {"insert into t8 values(1, 'Vasya', 'Gorod',  7.5);", "Success"}});
     std::vector<Request> request1{{"begin;", "Success"},
                                   {"update t8 set id = 10 where id = 1;", "Success"},
-                                  {"select * from t8;", ""},
+                                  {"select * from t8;",
+                                   "\nid|name   |city   |age     |\n"
+                                   "10|'Vasya'|'Gorod'|7.500000|\n"},
                                   {"commit;", "Success"}};
     std::vector<Request> request2{{"begin;", "Success"},
-                                  {"update t8 set id = 10 where id = 1;", "Success"},
-                                  {"select * from t8;", ""},
+                                  {"update t8 set id = 10 where id = 1;", "Table is in use ERROR: 14"},
+                                  {"select * from t8;",
+                                   "\nid|name   |city   |age     |\n"
+                                   "1 |'Vasya'|'Gorod'|7.500000|\n"},
                                   {"commit;", "Success"}};
     // в этом тесте должна быть ошибка, апдейт сразу в двух транзакциях
     std::thread client1(TestUtils::checkRequests, request1);
     std::thread client2(TestUtils::checkRequests, request2);
     client1.join();
     client2.join();
+    TestUtils::checkRequests({{"select * from t8;",
+                               "\nid|name   |city   |age     |\n"
+                               "10|'Vasya'|'Gorod'|7.500000|\n"}});
 }
